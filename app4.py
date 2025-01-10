930ffa1ec89771b5815be81efc94616386f70063
aldodavidhp <39342826+aldodavidhp@users.noreply.github.com>
2025-01-10 11:00:09 -0600

    Update app4.py

--- app4.py
+++ app4.py
@@ -48,8 +48,9 @@ def obtener_respuesta(mensaje_usuario, contexto):
     st.session_state.historial.append({"role": "user", "content": mensaje_usuario})
     st.session_state.historial.append({"role": "system", "content": f"Contexto: {contexto}"})
     openai.completions.create
-    #respuesta = openai.ChatCompletion.create(
-    respuesta = openai.chat_completions.create(
+    
+    #respuesta = openai.chat_completions.create(
+    respuesta = openai.ChatCompletion.create(
         model="gpt-4o-mini",  # Modelo para ChatGPT Plus
         #messages=st.session_state.historial,
         prompt=st.session_state.historial,
